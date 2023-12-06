a8432d86a8b5320d661370bc3b9fca8b97d0237d
Ashraf, Areeb <areebashraf@my.unt.edu>
2023-12-05 20:17:15 -0600

    Index Forcasting

---
+++ main.py
@@ -0,0 +1,78 @@
+# Imports
+import streamlit as st
+from datetime import date
+import yfinance as yf
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+# Get data
+START = "2017-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title('Index Forecasting')
+
+# Define a dictionary for mapping names to symbols
+index_mapping = {'Nasdaq Index': '^NDX', 
+'Nifty Index': '^NSEI',
+'Nasdaq Technology Index': '^NDXT', 
+'Nifty Technology Index': '^CNXIT', 
+# 'Nasdaq Healthcare Index': '^IXHC', 
+# 'Nifty Healthcare Index': 'NIFTY_HEALTHCARE.NS',
+# 'Nasdaq Energy Index': 'CELS', 
+# 'Nifty Energy Index': '^CNXENERGY',
+# 'Nasdaq Realestate Index': '^NQG35', 
+# 'Nifty Realestate Index': '^CNXREALTY'
+}
+
+# Select index name
+selected_name = st.selectbox('Select dataset for prediction', list(index_mapping.keys()))
+
+# Get the corresponding symbol
+selected_index = index_mapping[selected_name]
+
+# Get months of prediction
+n_months = st.slider('Months of prediction:', 1, 12)  
+period = n_months * 30  # Assuming an average of 30 days in a month
+ 
+# Cache data
+@st.cache_data
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+# Load data	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_index)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_months} month(s)')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
